d008529eea482e51e61f6a35ac4bd131761ca076
Lethallic <lethallic@gmail.com>
2015-06-17 16:17:22 +0000

    initial commit

--- role_transporter.js
+++ role_transporter.js
@@ -50,7 +50,7 @@ module.exports = {
 			*/
 			
 			console.log("ext", creep.room.extensions());
-			console.log("empty, "creep.room.emptyExtensions());
+			console.log("empty", creep.room.emptyExtensions());
 			
 			var extension = creep.pos.findClosest(creep.room.extensions(), {
 				filter : function(s) {
