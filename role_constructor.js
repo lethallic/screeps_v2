817b4fdb9d0137e9008291f8ea8211d217ece877
Lethallic <lethallic@gmail.com>
2015-06-17 10:45:52 +0000

    initial commit

--- role_constructor.js
+++ role_constructor.js
@@ -1,14 +1,19 @@
+var Debug = require("_debug");
 module.exports = {
 	
 	body : [MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, WORK],
 	
 	run : function(creep) {
+		var debug;
+		
 		
 		if ( creep.energy > 0 ) {
 			var target = null;
 			
 			if ( typeof creep.target() !== "undefined" ) {
 				// creep has current target
+				debug = new Debug("check target", 2);
+				
 				var target = Game.getObjectById(creep.target());
 				if ( target && target._type ) {
 					if ( target._type == "structure" && !target.needsRepair() ) {
@@ -17,25 +22,30 @@ module.exports = {
 				} else {
 				    target = null;
 				}
+				debug.log();
 			} 
 			
 			if  ( !target ) {
+				debug = new Debug("find constructions", 2);
 				var constructions = creep.room.find(FIND_CONSTRUCTION_SITES, {
 					filter : function(site) {
 						return true;
 					},
 					"algorithm" : "dijkstra"
 				});
+				debug.log();
 				
 				if ( constructions.length > 0 ) {
 					target = constructions[0];
 				} else {
+					debug = new Debug("find structures", 2);
 				    var structures = creep.room.find(FIND_STRUCTURES, {
 				        filter : function(s) {
 				            return s.needsRepair();
 				        },
 				        "algorithm" : "dijkstra"
 				    });
+				    debug.log();
 				    
 				    if ( structures.length ) {
 				        target = structures[0];
