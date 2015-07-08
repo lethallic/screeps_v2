a94ff88b36d5c177e54c5575e7dfabe0e1752f60
Lethallic <lethallic@gmail.com>
2015-07-08 16:14:33 +0000

    caching

--- main.js
+++ main.js
@@ -10,14 +10,7 @@ utils.log("------ NEW TICK ------");
 
 
 var GameController = require("controller_game");
-var gc = new GameController();
+var gc = utils.debugObject(new GameController());
 
-
-var dScout = utils.debug("Scout");
 gc.doScout();
-utils.log(dScout);
-
-
-var dRooms = utils.debug("gc.processRooms");
 gc.processRooms();
-utils.log(dRooms);
