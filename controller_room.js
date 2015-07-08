04bd7b205f51d7ee06c4f82a4ce8950f3c8f8a26
Lethallic <lethallic@gmail.com>
2015-07-08 14:57:06 +0000

    caching

--- controller_room.js
+++ controller_room.js
@@ -8,16 +8,23 @@ function RoomController(gameController, room) {
 utils.extend(RoomController.prototype, {
 
     produce: function() {
+        var debug = utils.debug("RoomController.process()");
+        
         this._controller.factory.produce(this._room, this._controller.roleManager);
+        
+        utils.log(debug);
     },
 
     doCreeps: function() {
+        var debug = utils.debug("RoomController.doCreeps()");
+        
         var creeps = this._room.getCreeps();
-
         for (var c in creeps) {
             var creep = creeps[c];
             this._controller.roleManager.process(creep);
         }
+        
+        utils.log(debug);
     }
 
 });
