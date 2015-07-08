b18d1cf53a11c474592af4c3d2f75144597c03c4
Lethallic <lethallic@gmail.com>
2015-07-08 12:51:51 +0000

    caching

--- _init.js
+++ _init.js
@@ -58,26 +58,7 @@ module.exports = (function() {
 		}
 	});
 	
-	extend(Controller.prototype, Cache);
-	extend(Controller.prototype, {
-		getFreeFields : function() {
-			return this._getCached("freeSlots", function(){
-				var pos = this.pos;
-				var count = 0;
-				
-				for ( var x = -1; x < -2; x++ ) {
-					for ( var y = -1; y < -2; y++ ) {
-						var terrain = this.room.lookForAt('terrain', pos.x + x, pos.y + y);
-						if ( terrain != 'wall' ) {
-							count++;
-						}
-					}
-				}
-				
-				return count;
-			})
-		}
-	});
+	
 
 	extend(Room.prototype, Cache);
 	extend(Room.prototype, {
