fa1ad59fd61bd48382fe69bd810b4ef17a1f4ddc
Lethallic <lethallic@gmail.com>
2015-07-08 14:50:56 +0000

    caching

--- _utils.js
+++ _utils.js
@@ -1,9 +1,29 @@
+var Debug = require("_debug");
+
 module.exports = {
     
     extend : function( proto , options ) {
     	for ( var key in options ) {
     		proto[key] = options[key];
     	}
-    }
+    },
+    
+    debug : function(name, minUsed) {
+        return new Debug(name, minUsed);
+    },
+    
+    log : function(debug) {
+        if ( this.getDebug() && debug ) {
+            debug.log;
+        }
+    },
+    
+    getDebug : function() {
+        return Memory['_debug'] || false;
+    },
+    
+    setDebug : function(debug) {
+        Memory['_debug'] = debug
+    },
     
 };
--- main.js
+++ main.js
@@ -4,7 +4,18 @@
 
 require("_init");
 
+var utils = require("_utils");
+
+
 var GameController = require("controller_game");
 var gc = new GameController();
+
+
+var dScout = utils.debug("Scout");
 gc.doScout();
+utils.log(debug);
+
+
+var dRoom = utils.debug("gc.processRooms");
 gc.processRooms();
+utils.log(dRooms);
