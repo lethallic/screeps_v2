6e6d039639c53f7100b91a962f16d168a93311d4
Lethallic <lethallic@gmail.com>
2015-07-10 07:14:23 +0000

    implementing links

--- role_constructor.js
+++ role_constructor.js
@@ -1,126 +1,140 @@
 var Debug = require("_debug");
 module.exports = {
-	
-	body : [MOVE, MOVE, MOVE, CARRY, CARRY, WORK],
-	body_small : [MOVE, CARRY, WORK],
-	body_big : [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, WORK, CARRY, CARRY, WORK],
-	
-	run : function(creep) {
+
+	body: [MOVE, MOVE, MOVE, CARRY, CARRY, WORK],
+	body_small: [MOVE, CARRY, WORK],
+	body_big: [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, WORK, CARRY, CARRY, WORK],
+
+	run: function(creep) {
 		var debug;
-		
+
 		var flags = _.filter(creep.room.getFlags(), function(f) {
 			return f.color == COLOR_BLUE;
 		});
-		if ( flags.length ) {
+		if (flags.length) {
 			creep.target("");
 			creep.moveTo(flags[0]);
 			return;
 		}
-		
-		
-		if ( creep.energy > 0 ) {
+
+
+		if (creep.energy > 0) {
 			var target = null;
-			
-			if ( typeof creep.target() !== "undefined" ) {
+
+			if (typeof creep.target() !== "undefined") {
 				// creep has current target
 				debug = new Debug("check target", 1);
-				
+
 				var target = Game.getObjectById(creep.target());
-				if ( target && target._type ) {
-					if ( target._type == "structure" && !target.needsRepair() ) {
+				if (target && target._type) {
+					if (target._type == "structure" && !target.needsRepair()) {
 						target = null;
 					}
-				} else {
-				    target = null;
+				}
+				else {
+					target = null;
 				}
 				debug.log();
-			} 
-			
-			if  ( !target ) {
+			}
+
+			if (!target) {
 				var constructions = creep.room.getConstructionSites();
-				if ( constructions.length > 0 ) {
+				if (constructions.length > 0) {
 					target = constructions[0];
-				} else {
-				    
-				 	var structures = _.filter(creep.room.getStructures(), function(s){
+				}
+				else {
+
+					var structures = _.filter(creep.room.getStructures(), function(s) {
 						return s.needsRepair();
 					});
-				    
-				    if ( structures.length ) {
-				    	target = _.min(structures, function(e) {
-				    	    var p = (e.hits * 100 / e.hitsMax);
-				    		return p;
-				    	});
-				    }
-				    
+
+					if (structures.length) {
+						target = _.min(structures, function(e) {
+							var p = (e.hits * 100 / e.hitsMax);
+							return p;
+						});
+					}
+
 				}
 			}
-			
-			if ( target ) {
+
+			if (target) {
 				// build / repair target
 				creep.moveTo(target);
-				if ( typeof target.progress == "number" ) {
+				if (typeof target.progress == "number") {
 					creep.build(target);
-				} else {
-				    creep.repair(target);
+				}
+				else {
+					creep.repair(target);
 				}
 				creep.target(target.id);
 				return;
 			}
 
-		} else if ( creep.energy == 0 ) {
+		}
+		else if (creep.energy == 0) {
 			// goto spawn, get energy
-			var spawn = creep.room.getSpawn();
-			if ( spawn ) {
-				creep.moveTo(spawn);
-				spawn.transferEnergy(creep);				
+			var links = spawn.pos.findInRange(spawn.room.getLinks(), 3);
+			var link = _.find(links, function(l) {
+				return (ll.energy > 0);
+			})
+
+			if (link) {
+				creep.moveTo(link);
+				link.transferEnergy
 			}
-			creep.target("");			
+			else {
+				creep.moveTo(spawn);
+				spawn.transferEnergy(creep);
+			}
+			creep.target("");
 		}
 	},
-	
-	
-	_doWork : function(creep) {
+
+
+	_doWork: function(creep) {
 		var target = _getTarget(creep);
 		var type = _getType(target);
-		
-		if ( energy == 0 ) {
-			
-		} else {
-			
+
+		if (energy == 0) {
+
 		}
-		
-		if ( target && creep.energy > 0 ) {
+		else {
+
+		}
+
+		if (target && creep.energy > 0) {
 			if (!_needsWork(target)) {
 				target = null;
 			}
-		} else {
+		}
+		else {
 			//  no energy
 			var energy = _getEnergy(creep);
-			if ( energy ) {
+			if (energy) {
 				target = energy;
 			}
 		}
 
-		if ( !target ) {
+		if (!target) {
 			// find target;
 		}
-		
-		if ( target ) {
+
+		if (target) {
 			creep.moveTo(target);
 			switch (_getType(target)) {
 				case 'spawn':
 					target.transferEnergy(creep);
 					break;
-					
+
 				case 'energy':
 					creep.pickup(target);
 					break;
-					
+
 				case 'structure':
 					creep.repair(target);
 					break;
-					
+
 				case 'construction':
 					creep.build(target);
 					break;
@@ -128,63 +142,68 @@ module.exports = {
 		}
 
 	},
-	
-	_getTarget : function(creep) {
-		if ( creep.target() ) {
+
+	_getTarget: function(creep) {
+		if (creep.target()) {
 			return Game.getObjectById(creep.target());
 		}
 		return null;
 	},
-	
-	_needsWork : function(target) {
-		if ( target.progress ) {
+
+	_needsWork: function(target) {
+		if (target.progress) {
 			return (target.progress < target.progressTotal);
-		} else if ( target.hits ) {
+		}
+		else if (target.hits) {
 			return (target.hits < target.hitsMax);
 		}
 		return false;
 	},
-	
-	_getEnergy : function(creep) {
+
+	_getEnergy: function(creep) {
 		var energy = this._findEnergy(creep);
-		if ( energy ) {
+		if (energy) {
 			// set target, get energy
 			creep.setTarget(energy.id)
 			return true;
-		} else {
+		}
+		else {
 			// goto spawn, get energy
 			var spawn = creep.room.getSpawn();
-			if ( spawn ) {
+			if (spawn) {
 				creep.setTarget(spawn.id)
 				return true;
 			}
 		}
 		return false;
 	},
-	
-	_findEnergy : function(creep) {
+
+	_findEnergy: function(creep) {
 		var energy = _.filter(creep.room.droppedEnergy(), function(e) {
-			return ( e.energy >= creep.energyCapacity );
+			return (e.energy >= creep.energyCapacity);
 		});
-		
-		if ( energy.length ) {
+
+		if (energy.length) {
 			return creep.pos.findClosest(energy);
 		}
-		
+
 		return null;
 	},
-	
-	_targetType : function(o) {
-		if ( o.createCreep ) {
+
+	_targetType: function(o) {
+		if (o.createCreep) {
 			return "spawn";
-		} else if ( o.progress ) {
+		}
+		else if (o.progress) {
 			return "construction";
-		} else if ( o.hits ) {
+		}
+		else if (o.hits) {
 			return "structure";
-		} else if ( o.energy ) {
+		}
+		else if (o.energy) {
 			return "energy";
 		}
 		return null;
 	}
-	
+
 };
