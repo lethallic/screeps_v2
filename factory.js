6f7a7e766dd634ff13d08c14c2a70e148e781b9c
Lethallic <lethallic@gmail.com>
2015-07-10 19:12:16 +0000

    transporter optimized

--- factory.js
+++ factory.js
@@ -57,8 +57,8 @@ module.exports = {
 	produce : function(room, roleManager) {
 		var sources = room.sources();
 		
-		if ( room.getCreeps().length < config.builders.max ) { // || room.maxEnergy() <= 600 ) {
-		//if ( room.maxEnergy() <= 600 ) {
+		// if ( room.getCreeps().length < config.builders.max ) { // || room.maxEnergy() <= 600 ) {
+		if ( room.maxEnergy() <= 600 ) {
 			// just build workers
 			var builders = room.getCreeps("builder");
 			
